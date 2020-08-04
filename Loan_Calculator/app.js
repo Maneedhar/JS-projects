9651169c50f61115358781855dcc1fc34c112fb8
Maneedhar <maneedharsharma@gmail.com>
2020-08-04 23:41:19 +0530

    Loan Calculator

--- Loan_Calculator/app.js
+++ Loan_Calculator/app.js
@@ -7,7 +7,7 @@ document.getElementById('loan-form').addEventListener('submit', function(e){
   //show loader
   document.getElementById('loading').style.display = 'block';
 
-  setTimeout(calculateResults, 2000);
+  setTimeout(calculateResults, 1000);
 
   e.preventDefault();
 })
